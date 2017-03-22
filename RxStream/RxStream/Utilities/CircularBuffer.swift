5e642dab426764bcc41ad56bb36ae6383ee841fa
Aaron Hayman <aaron@flexile.co>
2017-03-22 09:50:17 -0500

    - Added Docs and Unit Tests for Circular buffer - Fixed initial data not filling buffer

--- RxStream/RxStream/Utilities/CircularBuffer.swift
+++ RxStream/RxStream/Utilities/CircularBuffer.swift
@@ -8,22 +8,60 @@
 
 import Foundation
 
-struct CircularBuffer<T> : Collection {
-  private var buffer: [T] = []
+/**
+ A very simple Curcular Buffer designed to hold and return a statically limited number of elements without allocating, shifting or moving memory after the buffer has been initialized.
+ 
+ Data can be iterated, accessed and appended. The size of the buffer can't change and data cannot be deleted from the buffer.
+ */
+struct CircularBuffer<Element> : Collection {
+  /// Privately, we use a simple fixed array to store the collection and replace elements as needed, keeping track of the head index.
+  private var buffer: [Element] = []
+  
+  /// The size of the buffer.
   private let size: Int
+  
+  /// The current head index. -1 when the buffer is empty, otherwise it points to the start of the current buffer.
   private var head: Int = -1
   
   var count: Int { return buffer.count }
   var startIndex: Int { return 0 }
   var endIndex: Int { return buffer.endIndex }
   
-  init(size: Int, data: [T]? = nil) {
+  /**
+   Initialize the buffer with a size and optionally an initial set of data.
+   
+   - parameters:
+     - size: The size of the buffer.  Cannot be changed after initialization.
+     - data: _(Optional)_.  Provide an initial set of data to fill the buffer with. If you provide data that is larger than the buffer, only the last `n == size` elements will be added to the buffer.
+  
+   - returns: A new CircularBuffer
+  */
+  init(size: Int, data: [Element]? = nil) {
     self.size = Swift.max(1, size)
+    // Fill buffer if data is provided
+    if let newData = data {
+      switch newData.count {
+      case 0: break
+      case 1...size:
+        buffer = newData
+        head = 0
+      default:
+        buffer = newData[(newData.count - size)..<newData.count].map{ $0 }
+        head = 0
+      }
+    }
     self.buffer.reserveCapacity(self.size)
   }
   
-  subscript(index: Int) -> T {
-    guard index < size else { fatalError() }
+  /**
+   Retrieve an element from the buffer by index.
+   
+   - parameter index: The index of the element you wish to retrieve.  If you provide an index outside of the buffer range, a fatal error will be thrown.
+   
+   - returns: The element at the specified index.
+   */
+  subscript(index: Int) -> Element {
+    guard index < buffer.count else { fatalError("CircularBuffer: Index outside of buffer range.") }
     guard buffer.count >= size else { return buffer[index] }
     let offset = head + index
     if offset < size {
@@ -33,16 +71,23 @@ struct CircularBuffer<T> : Collection {
     }
   }
   
-  mutating func append(_ item: T) {
+  /**
+   Append a new element to the buffer.
+   If the buffer is full, the element will overwrite the oldest element in the buffer.  
+   Otherwise, the element will be appended to the current buffer data.
+   
+   - parameter element: The element to add.
+   */
+  mutating func append(_ element: Element) {
     switch (head, buffer.count) {
     case (_, 0..<size):
-      buffer.append(item)
+      buffer.append(element)
       head = 0
     case (0..<size - 1, _):
-      buffer[head] = item
+      buffer[head] = element
       head += 1
     case (size - 1, _):
-      buffer[head] = item
+      buffer[head] = element
       head = 0
     default: fatalError("Internal Error: Either the buffer has overflown or the head is out of range.")
     }
---
+++ RxStream/RxStreamTests/CircularBufferTests.swift
@@ -0,0 +1,111 @@
+//
+//  CircularBufferTests.swift
+//  RxStream
+//
+//  Created by Aaron Hayman on 3/22/17.
+//  Copyright © 2017 Aaron Hayman. All rights reserved.
+//
+
+import XCTest
+@testable import Rx
+
+class CircularBufferTests: XCTestCase {
+  
+  func testAppendingElements() {
+    var buffer = CircularBuffer<Int>(size: 3)
+    XCTAssertEqual(buffer.count, 0)
+    
+    buffer.append(0)
+    XCTAssertEqual(buffer.count, 1)
+    XCTAssertEqual(buffer[0], 0)
+    
+    buffer.append(1)
+    XCTAssertEqual(buffer.count, 2)
+    XCTAssertEqual(buffer[0], 0)
+    XCTAssertEqual(buffer[1], 1)
+    
+    buffer.append(2)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 0)
+    XCTAssertEqual(buffer[1], 1)
+    XCTAssertEqual(buffer[2], 2)
+    
+    buffer.append(3)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 1)
+    XCTAssertEqual(buffer[1], 2)
+    XCTAssertEqual(buffer[2], 3)
+    
+    buffer.append(4)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 2)
+    XCTAssertEqual(buffer[1], 3)
+    XCTAssertEqual(buffer[2], 4)
+    
+    buffer.append(5)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 3)
+    XCTAssertEqual(buffer[1], 4)
+    XCTAssertEqual(buffer[2], 5)
+  }
+  
+  func testInitialDataLessThanBuffer() {
+    var buffer = CircularBuffer(size: 3, data: [0, 1])
+    XCTAssertEqual(buffer.count, 2)
+    XCTAssertEqual(buffer[0], 0)
+    XCTAssertEqual(buffer[1], 1)
+    
+    buffer.append(2)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 0)
+    XCTAssertEqual(buffer[1], 1)
+    XCTAssertEqual(buffer[2], 2)
+    
+    buffer.append(3)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 1)
+    XCTAssertEqual(buffer[1], 2)
+    XCTAssertEqual(buffer[2], 3)
+  }
+  
+  func testInitialDataEqualToBuffer() {
+    var buffer = CircularBuffer(size: 3, data: [0, 1, 2])
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 0)
+    XCTAssertEqual(buffer[1], 1)
+    XCTAssertEqual(buffer[2], 2)
+    
+    buffer.append(3)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 1)
+    XCTAssertEqual(buffer[1], 2)
+    XCTAssertEqual(buffer[2], 3)
+    
+    buffer.append(4)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 2)
+    XCTAssertEqual(buffer[1], 3)
+    XCTAssertEqual(buffer[2], 4)
+  }
+  
+  func testInitialDataGreaterThanBuffer() {
+    var buffer = CircularBuffer(size: 3, data: [0, 1, 2, 3, 4])
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 2)
+    XCTAssertEqual(buffer[1], 3)
+    XCTAssertEqual(buffer[2], 4)
+    
+    buffer.append(5)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 3)
+    XCTAssertEqual(buffer[1], 4)
+    XCTAssertEqual(buffer[2], 5)
+    
+    buffer.append(6)
+    XCTAssertEqual(buffer.count, 3)
+    XCTAssertEqual(buffer[0], 4)
+    XCTAssertEqual(buffer[1], 5)
+    XCTAssertEqual(buffer[2], 6)
+  }
+    
+}
